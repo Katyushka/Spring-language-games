63115c3a5fd19c9e584667eaa8e72bba8c739904
KaVaBUnGO <kavabungo1@gmail.com>
2016-04-12 01:10:41 +0500

    add success url

--- src/main/java/com/language/config/SecurityConfig.java
+++ src/main/java/com/language/config/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .loginPage("/login")
                 .failureUrl("/login?error")
+                .defaultSuccessUrl("/user")
                 .permitAll()
                 .and()
                 .logout()
